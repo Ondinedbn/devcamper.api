a0c9cc2cf49ad3f026533a4551dc0a1c236f8ff1
Ondine Debienne <ondine.debienne@gmail.com>
2023-07-18 14:23:33 +0200

    Routes, controller

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(
   PORT,
