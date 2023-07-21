35fdacae4aaed7ae31a3dbfc4fb82e3094142a79
Ondine Debienne <ondine.debienne@gmail.com>
2023-07-21 14:43:47 +0200

    Bootcamp user ownership

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -26,7 +27,8 @@ const courses = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    // await Course.create(courses);
+    await Course.create(courses);
+    await User.create(users);
     console.log("Data imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -39,6 +41,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data destroyed...".red.inverse);
     process.exit();
   } catch (err) {
