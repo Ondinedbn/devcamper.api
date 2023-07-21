72a698b973b98cbf7dfff81c07e8eed0c33ff861
Ondine Debienne <ondine.debienne@gmail.com>
2023-07-21 11:24:44 +0200

    Added protect middleware for routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,24 @@ const sendTokenResponse = (user, statusCode, res) => {
     httpOnly: true,
   };
 
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     token,
   });
 };
+
+// @description     Get current logged in user
+// @route           POST /api/v1/auth/me
+// @access          Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    sucess: true,
+    data: user,
+  });
+});
