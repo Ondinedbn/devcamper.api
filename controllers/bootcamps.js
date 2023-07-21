ea94984cdc1ac6792c330e3cc536d3e095aa7988
Ondine Debienne <ondine.debienne@gmail.com>
2023-07-21 14:52:52 +0200

    Validation of bootcamp ownership for operations

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -53,14 +53,29 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route           PUT /api/v1/bootcamps/:id
 // @access          Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+  let bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
 
-  if (!bootcamp) {
-    new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404);
-  }
   res.status(200).json({ success: true, data: bootcamp });
 });
 
@@ -69,8 +84,18 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access          Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
+
   if (!bootcamp) {
     new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404);
+  }
+
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete this bootcamp`,
+        401
+      )
+    );
   }
 
   bootcamp.remove();
@@ -111,9 +136,19 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 // @access          Private
 exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
+
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
     );
   }
 
